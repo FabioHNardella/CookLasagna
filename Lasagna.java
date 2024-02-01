514180fa74e6f93cefdaab9c21f228eafcfaee29
Fubin <102842907+Fubinnn@users.noreply.github.com>
2024-02-01 03:18:46 -0300

    Add files via upload

---
+++ Lasagna.java
@@ -0,0 +1,37 @@
+package packageSketch;
+
+public class Lasagna {
+    // TODO: define the 'expectedMinutesInOven()' method
+    public void expectedMinutesInOven(){
+        System.out.println(40);
+}// end of method "expec min in"
+        
+    // TODO: define the 'remainingMinutesInOven()' method
+    public void remainingMinutesInOven(int a){
+        int b = 40 - a;
+    	System.out.println(b);
+}// end of method "min in"
+    
+    
+    // TODO: define the 'preparationTimeInMinutes()' method
+    public void preparationTimeInMinutes(int x){
+    	int y = x * 2;
+    	System.out.println(y);
+}// end of "prep in min"
+    
+    
+    // TODO: define the 'totalTimeInMinutes()' method
+    	public void totalTimeInMinutes(int a, int b) {
+    		int c = a * 2 + b;
+    		System.out.println(c);
+    		
+    		
+}// end of "total time in min"
+
+
+
+
+
+
+
+}
